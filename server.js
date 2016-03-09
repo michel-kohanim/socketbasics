dafc12e270c6e5332331c16ba331addac195e82f
MKohanim <michel.kohanim@gmai.com>
2016-03-09 10:43:57 -0800

    socket.io and jquery

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = new express();
-var http = require('http').Server(app); 
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname+'/public'));
+
+io.on('connection' , function(){
+	console.log('User connected via socket.io');
+}) //listen for events
 
 http.listen(PORT, function(){
 	console.log("Server started on port " + PORT);
