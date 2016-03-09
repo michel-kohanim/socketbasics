274aa402054fe7deb30c0315f17a7c2cc24cb92c
MKohanim <michel.kohanim@gmai.com>
2016-03-09 12:04:34 -0800

    text form

--- public/js/app.js
+++ public/js/app.js
@@ -10,3 +10,17 @@ socket.on('message', function(message){
 	console.log('New mesage:');
 	console.log(message.text);
 });
+
+//handles submitting a new message
+var $form = jQuery('#message-form'); //select the element in the DOM ID need #
+
+//this will get called with form is submitted
+$form.on('submit', function(event){
+	var $message = $form.find('input[name=message]'); //.val() //find message within the form
+	event.preventDefault();	 //used on the form when you don't want to refresh the entire page
+	socket.emit('message', {
+		text: $message.val() //find message within the form
+	});
+	$message.val(''); //clear the form
+
+});
